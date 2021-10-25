e8a2762996482a85e34a50586fd72ccd18343048
AquiLiusOP <adibmannanrafi2003@gmail.com>
2021-10-25 23:27:35 +0600

    2nd commit

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import img from '../../images/6M513.png'
 const NotFound = () => {
     return (
         <div>
-            <h1>Error 404</h1>
+            <img src={img} alt="" />
         </div>
     );
 };
